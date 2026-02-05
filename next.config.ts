ca1665db918d65fb40dc5dee195e9114cc1afea3
HashenWatawala <hashanewatawala@gmail.com>
2026-02-05 16:11:46 +0530

    Deplyemet file created

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
-  output: 'standalone',
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
         protocol: 'https',
